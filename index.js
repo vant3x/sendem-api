542323d8c1fce39ee823f46345503c979c3e63a0
Alejandro <Velasquez>
2021-02-03 20:29:24 -0500

    fix cors

--- index.js
+++ index.js
@@ -15,6 +15,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 // Puerto de la app
 const port = process.env.PORT || 4000;
 
