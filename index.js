f8b7179c9068df2713a23834a92b36736d69aac4
Alejandro <Velasquez>
2021-02-04 15:11:29 -0500

    set headers for cors

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ app.use(cors(corsOptions));
 
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    res.setHeader("Access-Control-Allow-Origin", `${proces.env.FRONTEND_URL}`);
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.setHeader("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
     next();
 });
 
